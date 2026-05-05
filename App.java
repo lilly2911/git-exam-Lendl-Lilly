60b797e63862d483621e4f73e21be0dc6b709012
lilly2911 <lenlid23@htl-kaindorf.at>
2026-05-05 10:59:49 +0200

    baseline file corrected

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
-public static void main(String[] args)
-{
-	System.out.println("System Initialized");
+public class App{
+
+	public static void main(String[] args)
+	{
+		System.out.println("System Initialized");
+	}
 }
